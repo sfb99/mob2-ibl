c3e6dfbc8d4559b4e73884294008c01cca7bb2da
williamium <william.walker@bbc.co.uk>
2018-10-16 09:40:36 +0100

    Added carousel object to whats on response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // Import the Dialogflow module and response creation dependencies
 // from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const {dialogflow, Image, Carousel} = require('actions-on-google');
 
 const client = require('@ibl/client').createClient();
 
@@ -18,19 +18,33 @@ app.intent('Default Welcome Intent', (conv) => {
   console.log("ARE YOU HITTING ME, BABY?");
   
   conv.ask(`G'day cobber. What on B.B.C. T.V. can I help you with?`);
-
 });
 
 app.intent('whats popular', async (conv) => {
   let results = await client.getPopularEpisodes();
   let popular = "The top favourite shows are: ";
+  let carousel = { items: {} };
   for(var i = 0; i < results.elements.length; i++)
   {
     if (i > 5) break;
     if (i > 0) popular += ", ";
+    carousel.items[results.elements[i].id] = {
+      synonyms: [
+        results.elements[i].title
+      ],
+      title: results.elements[i].title + ' ' + results.elements[i].subtitle,
+      description: results.elements[i].synopses.small,
+      image: new Image({
+        url: results.elements[i].images.standard,
+        alt: results.elements[i].title,
+      }),
+    };
     popular += results.elements[i].title;
   }
-  conv.close(popular);
+
+  // conv.close(popular);
+  conv.ask(popular);
+  conv.ask(new Carousel(carousel));
 })
 
 app.intent('Default Fallback Intent', (conv) => {
